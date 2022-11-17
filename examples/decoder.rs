5f77a12960c8a044f1b446909fd06bc664a05cf4
Felix Kohlgrüber <felix.kohlgrueber@gmail.com>
2022-11-17 21:54:22 +0100

    add decoder example

---
+++ examples/decoder.rs
@@ -0,0 +1,35 @@
+use std::io::Read;
+
+/// Reads bytes from stdin and prints decoded messages / errors
+///
+/// Example usage:
+///
+/// ```
+/// cat tests/libsml-testing/dzg_dwsb20_2th_3byte.bin | cargo run --all-features --example decoder
+/// ```
+
+fn main() -> Result<(), std::io::Error> {
+    let stdin = std::io::stdin().lock();
+
+    let mut decoder = sml_rs::transport::Decoder::<Vec<u8>>::new();
+
+    for res in stdin.bytes() {
+        let b = res?;
+
+        match decoder.push_byte(b) {
+            Ok(None) => {}
+            Ok(Some(decoded_bytes)) => {
+                println!("Ok({})", decoded_bytes.len());
+            }
+            Err(e) => {
+                println!("Err({:?})", e);
+            }
+        }
+    }
+
+    if let Some(e) = decoder.finalize() {
+        println!("Err({:?})", e);
+    }
+
+    Ok(())
+}
