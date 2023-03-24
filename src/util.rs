4aeaddd4bdf2135e6b71ae60ab5d660f4853cf59
Felix Wirth <felix@wirth.one>
2023-03-24 14:22:54 +0100

    tests; small fixes

--- src/util.rs
+++ src/util.rs
@@ -11,8 +11,8 @@ pub type VecBuf = alloc::vec::Vec<u8>;
 /// Interface for byte vectors.
 ///
 /// This train provides is used as an abstraction over different byte vector
-/// implementations. It is implemented for static vectors (`heapless::Vec<u8>`)
-/// and (if the `alloc` feature is used( for dynamic vectors (`alloc::Vec<u8>`).
+/// implementations. It is implemented for static vectors (`ArrayBuf`)
+/// and (if the `alloc` feature is used) for dynamic vectors (`alloc::Vec<u8>`).
 pub trait Buffer: Default + Deref<Target = [u8]> {
     /// Appends a byte to the back of the vector.
     ///
@@ -139,3 +139,78 @@ impl<const N: usize> Buffer for ArrayBuf<N> {
 /// Error type indicating that an operation failed due to lack of memory.
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct OutOfMemory;
+
+#[cfg(test)]
+mod test_arraybuf {
+    use crate::util::{Buffer, OutOfMemory};
+
+    use super::ArrayBuf;
+
+    #[test]
+    fn test_basic() {
+        let mut buf: ArrayBuf<5> = (0..3).collect();
+        assert_eq!(&format!("{:?}", buf), "[0, 1, 2]");
+        assert_eq!(buf.len(), 3);
+        assert_eq!(buf.push(10), Ok(()));
+        assert_eq!(buf.len(), 4);
+        assert_eq!(buf.push(20), Ok(()));
+        assert_eq!(buf.len(), 5);
+        assert_eq!(buf.push(30), Err(OutOfMemory));
+        assert_eq!(buf.len(), 5);
+        assert_eq!(&*buf, &[0, 1, 2, 10, 20]);
+        buf.truncate(1000);
+        assert_eq!(&*buf, &[0, 1, 2, 10, 20]);
+        buf.truncate(1);
+        assert_eq!(&*buf, &[0]);
+        buf.truncate(0);
+        assert_eq!(&*buf, &[]);
+        assert_eq!(buf.extend_from_slice(&[7, 6, 5, 4, 3]), Ok(()));
+        assert_eq!(&*buf, &[7, 6, 5, 4, 3]);
+        buf.truncate(1);
+        assert_eq!(&*buf, &[7]);
+        assert_eq!(buf.extend_from_slice(&[10, 11]), Ok(()));
+        assert_eq!(&*buf, &[7, 10, 11]);
+        assert_eq!(buf.extend_from_slice(&[25, 26, 27]), Err(OutOfMemory));
+        buf.clear();
+        assert_eq!(&*buf, &[]);
+    }
+
+    #[test]
+    fn test_from_iter() {
+        let buf: ArrayBuf<5> = (0..3).collect();
+        assert_eq!(&*buf, &[0, 1, 2]);
+        let buf: ArrayBuf<3> = (0..3).collect();
+        assert_eq!(&*buf, &[0, 1, 2]);
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_from_panic() {
+        let _: ArrayBuf<2> = (0..3).collect();
+    }
+
+    #[test]
+    fn test_eq() {
+        let mut buf_a: ArrayBuf<5> = (0..3).collect();
+        let mut buf_b: ArrayBuf<5> = (0..3).collect();
+        assert_eq!(buf_a, buf_b);
+        buf_b.push(123).unwrap();
+        assert_ne!(buf_a, buf_b);
+        buf_b.push(199).unwrap();
+        assert_ne!(buf_a, buf_b);
+        buf_a.truncate(3);
+        buf_b.truncate(3);
+        assert_eq!(buf_a, buf_b);
+        buf_a.clear();
+        assert_ne!(buf_a, buf_b);
+        buf_b.clear();
+        assert_eq!(buf_a, buf_b);
+    }
+
+    #[test]
+    fn test_n0() {
+        let mut buf = ArrayBuf::<0>::default();
+        assert_eq!(buf.len(), 0);
+        assert_eq!(buf.push(30), Err(OutOfMemory));
+    }
+}
